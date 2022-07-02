a7677c03f247d65ef17d27d7395d29c3121eeb42
Zows <vyudchenko@gmail.com>
2022-07-02 14:16:08 +0300

    Colors

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+	typing animation
+ */
+var typed = new Typed(".typing",{
+	strings: ["", "Web Designer", "Web Debeloper", "Graphic Designer"],
+	typeSpeed: 100,
+	BackSpeed: 60,
+	loop: true
+})
