77596b2dd7f3e32c86fb4db5400f10c45940fada
Viktor Yudchenko <vyudchenko@gmail.com>
2022-07-11 12:00:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 	typing animation
  */
 var typed = new Typed(".typing",{
-	strings: ["", "Web Designer", "Web Debeloper", "Graphic Designer"],
+	strings: ["", "Web Designer", "Web Developer", "Graphic Designer"],
 	typeSpeed: 100,
 	BackSpeed: 60,
 	loop: true
